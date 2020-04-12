9f4534cb60bbc907e6a75e4c0c376ac4a541e2d7
texatron <teratronsirimus@gmail.com>
2020-04-12 14:13:51 +0300

    Функция производной активации

--- neuralnetwork.go
+++ neuralnetwork.go
@@ -25,7 +25,7 @@ func main() {
 	indLayer	:= numLayer - 1						// Индекс последнего слоя нейросети
 	layer 		:= make([]NeuralLayer, numLayer)	// Создаём срез нейронных слоёв
 
-	// Первая проходка по нейронным слоям
+	// Инициализация нейронных слоёв
 	for i = 0; i < numLayer; i++ {
 		// Создаем срезы для структуры нейронных слоёв
 		layer[i].Node   = make([]float32,   numNeuron[i])
@@ -45,28 +45,27 @@ func main() {
 	}
 
 	// Считаем значения нейронов в слое
-	layer[0].calcNeuron(&inputSet)
+	layer[0].calcNeuron(numNeuron[0], &inputSet)
 	for i = 1; i < numLayer; i++ {
-		layer[i].calcNeuron(&layer[i - 1].Node)
+		layer[i].calcNeuron(numNeuron[i], &layer[i - 1].Node)
 	}
 
 	// Вычисляем ошибки между обучающим набором и полученными выходными нейронами
 	for i = 0; i < numNeuron[indLayer]; i++ {
-		layer[indLayer].Error[i] = (dataSet[i] - layer[indLayer].Node[i]) * layer[indLayer].Node[i] * (1 - layer[indLayer].Node[i])
+		layer[indLayer].Error[i] = (dataSet[i] - layer[indLayer].Node[i]) * getDerivativeActivation(layer[indLayer].Node[i])
 	}
 	//e = (float32)(math.Pow((float64)(), 2))
 
 	// Вычисляем ошибки нейронов в скрытых слоях
 	for i = indLayer; i > 0; i-- {
-		j = i - 1
-		layer[j].Error = layer[i].calcError(&layer[j].Node)
+		layer[i - 1].calcError(&layer[i])
 	}
 
 	// Обновление весов
 	//$W[$x,$y] = $W[$x,$y] + $k * $No[$y,1] * $Ni[$x,0] * $No[$y,0] * (1-$No[$y,0])
 	//$W[$x,$y] = $W[$x,$y] + $k * $No[$y,1] * $Ni[$x,0]
 
-	//Weight = Weight + ratioLearn * y * (1 - y) * inputSet
+	//Weight = Weight + ratioLearn * getDerivativeActivation(y) * inputSet
 
 	// Вывод значений нейросети
 	printLayer(&layer)
@@ -82,33 +81,35 @@ func (layer *NeuralLayer) fillWeight() {
 	}
 }
 
+//
+func d() {
+
+}
+
 // Функция вычисления значения нейронов в слое
-func (layer *NeuralLayer) calcNeuron(node *[]float32) {
+func (layer *NeuralLayer) calcNeuron(num int, node *[]float32) {
 	l := *layer
 	n := *node
-	for i := range l.Node {
+	for i := 0; i < num; i++ {
 		var sum float32 = 0
-		for j := range l.Weight[i] {
-			sum += n[j] * l.Weight[i][j]
+		for j, w:= range l.Weight[i] {
+			sum += n[j] * w
 		}
 		l.Node[i] = getNeuralActivation(sum, 0)
 	}
 }
 
 // Функция вычисления ошибки нейронов в скрытых слоях
-func (layer *NeuralLayer) calcError(node *[]float32) []float32 {
+func (layer *NeuralLayer) calcError(node *NeuralLayer) {
 	l := *layer
 	n := *node
-	k := len(n)
-	e := make([]float32, k)
-	for i := 0; i < k; i++ {
+	for i, v := range l.Node {
 		var sum float32 = 0
-		for j := range l.Error {
-			sum += l.Error[j] * l.Weight[j][i]
+		for j, e := range n.Error {
+			sum += e * n.Weight[j][i]
 		}
-		e[i] = sum * n[i] * (1 - n[i])
+		l.Error[i] = sum * getDerivativeActivation(v)
 	}
-	return e
 }
 
 // Функция активации нейрона
@@ -123,6 +124,11 @@ func getNeuralActivation(sum float32, mode uint8) float32 {
 		default:	  return sum
 		}
 	}
+}
+
+// Функция производной активации
+func getDerivativeActivation(y float32) float32 {
+	return y * (1 - y)
 }
 
 // Функция вывода результатов нейросети
