c34db9eb6511343df93b33be29a3ef3ee29d3869
texatron <teratronsirimus@gmail.com>
2020-04-14 15:07:14 +0300

    test

--- test.go
+++ test.go
@@ -12,6 +12,7 @@ type NNMatrix struct {
 
 type NNLayer struct {
 	Size	int			// Количество нейронов в слое
+	Bias	float32		// Нейрон смещения
 	Node	[]NNNode	// Коллекция полей нейрона
 }
 
@@ -32,10 +33,7 @@ func main() {
 		Layer:	[]NNLayer{
 			{ // 1 - Входной слой
 				Size: 2,
-				Node: []NNNode{
-					{Error: nil},
-					{Error: nil},
-				},
+				Node: []NNNode{},
 			},
 			{ // 2
 				Size: 5,
@@ -57,6 +55,7 @@ func main() {
 
 	layer := make([]NNLayer, matrix.Size)
 	layer = []NNLayer{
+		{Size: 2, Bias: 1, Node: []NNNode{{0.4, 0.5}, {0.4, 0.5}}},
 		{
 			Size: 5,
 			Node: []NNNode{
@@ -67,19 +66,30 @@ func main() {
 				{0.5, 0.6},
 			},
 		},
-		{Size: 4, Node: []NNNode{}},
-		{2, []NNNode{}},
+		{Size: 4, Bias: 1, Node: []NNNode{}},
+		{Size: 2, Bias: 0, Node: []NNNode{}},
 	}
 
-	fmt.Println(layer[0].Node[0].Neuron, matrix)
-	//layer.Node
-	//matrix := Matrix{}
-	/*layer  := make([]Layer, 5)
-	node  :=
-	//node   := Node
-	layer[0].Size = 5
-	layer[0].Node*/
-	//var node Neuron
+	weight := make([]NNWeight, matrix.Size - 1)
+	weight = []NNWeight{
+		{
+			Weight: [][]float32{
+				{0.1, 0.2, 0.3, 0.4, 0.5},
+				{0.1, 0.2, 0.3, 0.4, 0.5},
+			},
+		},
+		{Weight: [][]float32{{0.1, 0.2, 0.3, 0.4, 0.5}, {0.1, 0.2, 0.3, 0.4, 0.5}}},
+		{Weight: [][]float32{{0.1, 0.2, 0.3, 0.4, 0.5}, {0.1, 0.2, 0.3, 0.4, 0.5}}},
+	}
 
-	//fmt.Println(layer[0].Node," ",node)
+	fmt.Println(layer[0].Node[0].Neuron)
+	fmt.Println(matrix)
+	fmt.Println(weight[1].Weight[0][0])
+
+	//matrix.Layer[0].Node[0].Neuron = 0.1
+
+	//matrix1 := matrix.layer
+
+	//fmt.Println(matrix{Layer: layer[0]}) //.Neuron= copy(matrix.Layer[0].Node[0].Neuron, matrix.Input)
+
 }
