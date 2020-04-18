4ae54793267fb507872366dda4e39baf21a2d384
texatron <teratronsirimus@gmail.com>
2020-04-18 17:21:44 +0300

    write file

--- nn.go
+++ nn.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"./lib"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"math"
 	"math/rand"
+	"strconv"
 )
 
 // Коллекция параметров матрицы
@@ -32,7 +36,7 @@ type NNWeight struct {
 func main() {
 	var (
 		collision	float32
-		bias		float32	= 1
+		bias		float32	= 0
 		ratio		float32	= .5
 		input	= []float32{1.2, 6.3}	// Входные параметры
 		data	= []float32{6.3, 3.2}	// Обучающий набор с которым будет сравниваться выходной слой
@@ -41,27 +45,34 @@ func main() {
 
 	// Инициализация нейронных слоёв и весов (матрицы)
 	matrix := NNMatrix{}
-	matrix.initMatrix(bias, ratio, input, data, hidden)
+	matrix.InitNN(bias, ratio, input, data, hidden)
 
 	// Обучение нейронной сети за какое-то количество эпох
 	for i := 0; i < 1000; i++ {
-		matrix.calcNeuron()						// Вычисляем значения нейронов в слое
-		collision = matrix.calcOutputError()	// Вычисляем ошибки между обучающим набором и полученными выходными нейронами
-		matrix.calcError()						// Вычисляем ошибки нейронов в скрытых слоях
-		matrix.updateWeight()					// Обновление весов
+		matrix.CalcNeuron()                  // Вычисляем значения нейронов в слое
+		collision = matrix.CalcOutputError() // Вычисляем ошибки между обучающим набором и полученными выходными нейронами
+		matrix.CalcError()                   // Вычисляем ошибки нейронов в скрытых слоях
+		matrix.UpdWeight()                   // Обновление весов
+	}
+
+	fmt.Println(lib.Get("cs"))
+
+	err := matrix.WriteWeight()
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	// Вывод значений нейросети
-	matrix.printMatrix(collision)
+	matrix.PrintNN(collision)
 }
 
 //
-func getNN() {
+func Get() {
 
 }
 
 // Функция инициализации матрицы
-func (matrix *NNMatrix) initMatrix(bias float32, ratio float32, input []float32, data []float32, hidden []int) {
+func (matrix *NNMatrix) InitNN(bias float32, ratio float32, input []float32, data []float32, hidden []int) {
 	var i, j, index int
 	layer := []int{len(input)}
 	for _, v := range hidden {
@@ -96,7 +107,7 @@ func (matrix *NNMatrix) initMatrix(bias float32, ratio float32, input []float32,
 				matrix.Weight[i].Weight[j] = make([]float32, matrix.Weight[i].Size[1])
 			}
 			// Заполняем все веса случайными числами от -0.5 до 0.5
-			matrix.Weight[i].fillWeight(matrix.Bias)
+			matrix.Weight[i].FillRandWeight(matrix.Bias)
 		}
 	}
 	copy(matrix.Layer[0].Neuron, input)
@@ -104,7 +115,7 @@ func (matrix *NNMatrix) initMatrix(bias float32, ratio float32, input []float32,
 }
 
 // Функция заполняет все веса случайными числами от -0.5 до 0.5
-func (weight *NNWeight) fillWeight(bias float32) {
+func (weight *NNWeight) FillRandWeight(bias float32) {
 	n := weight.Size[0] - 1
 	for i := 0; i < weight.Size[0]; i++ {
 		for j := 0; j < weight.Size[1]; j++ {
@@ -117,7 +128,7 @@ func (weight *NNWeight) fillWeight(bias float32) {
 }
 
 // Функция вычисления значения нейронов в слое
-func (matrix *NNMatrix) calcNeuron() {
+func (matrix *NNMatrix) CalcNeuron() {
 	for i := 1; i < matrix.Size; i++ {
 		n := i - 1
 		for j := 0; j < matrix.Layer[i].Size; j++ {
@@ -125,37 +136,37 @@ func (matrix *NNMatrix) calcNeuron() {
 			for k, v := range matrix.Layer[n].Neuron {
 				sum += v * matrix.Weight[n].Weight[k][j]
 			}
-			matrix.Layer[i].Neuron[j] = getActivation(sum, 0)
+			matrix.Layer[i].Neuron[j] = GetActivation(sum, 0)
 		}
 	}
 }
 
 // Функция вычисления ошибки выходного нейрона
-func (matrix *NNMatrix) calcOutputError() (collision float32) {
+func (matrix *NNMatrix) CalcOutputError() (collision float32) {
 	collision = 0
 	j := matrix.Size - 1
 	for i, v := range matrix.Layer[j].Neuron {
-		matrix.Layer[j].Error[i] = (matrix.Data[i] - v) * getDerivative(v, 0)
+		matrix.Layer[j].Error[i] = (matrix.Data[i] - v) * GetDerivative(v, 0)
 		collision += (float32)(math.Pow((float64)(matrix.Layer[j].Error[i]), 2))
 	}
 	return collision
 }
 
 // Функция вычисления ошибки нейронов в скрытых слоях
-func (matrix *NNMatrix) calcError() {
+func (matrix *NNMatrix) CalcError() {
 	for i := matrix.Size - 2; i > 0; i-- {
 		for j := 0; j < matrix.Layer[i].Size; j++ {
 			var sum float32 = 0
 			for k, v := range matrix.Layer[i + 1].Error {
 				sum += v * matrix.Weight[i].Weight[j][k]
 			}
-			matrix.Layer[i].Error[j] = sum * getDerivative(matrix.Layer[i].Neuron[j], 0)
+			matrix.Layer[i].Error[j] = sum * GetDerivative(matrix.Layer[i].Neuron[j], 0)
 		}
 	}
 }
 
 // Функция обновления весов
-func (matrix *NNMatrix) updateWeight() {
+func (matrix *NNMatrix) UpdWeight() {
 	for i := 1; i < matrix.Size; i++ {
 		n := i - 1
 		for j, v := range matrix.Layer[i].Error {
@@ -163,38 +174,38 @@ func (matrix *NNMatrix) updateWeight() {
 				if k == matrix.Layer[n].Size && matrix.Bias == 0 {
 					continue
 				}
-				matrix.Weight[n].Weight[k][j] += matrix.Ratio * v * p * getDerivative(matrix.Layer[i].Neuron[j], 0)
+				matrix.Weight[n].Weight[k][j] += matrix.Ratio * v * p * GetDerivative(matrix.Layer[i].Neuron[j], 0)
 			}
 		}
 	}
 }
 
 // Функция активации нейрона
-func getActivation(val float32, mode uint8) float32 {
+func GetActivation(value float32, mode uint8) float32 {
 	switch mode {
 	default: fallthrough
-	case 0: return (float32)(1 / (1 + math.Pow(math.E, (float64)(-val)))) // Sigmoid
+	case 0: return (float32)(1 / (1 + math.Pow(math.E, (float64)(-value)))) // Sigmoid
 	case 1: // Leaky ReLu
 		switch {
-		case val < 0: return 0.01 * val
-		case val > 1: return 1 + 0.01 * (val - 1)
-		default:	  return val
+		case value < 0: return 0.01 * value
+		case value > 1: return 1 + 0.01 * (value - 1)
+		default:	  return value
 		}
-	case 2: return (float32)(2 / (1 + math.Pow(math.E, (float64)(-2 * val))) - 1) // Tanh - гиперболический тангенс
+	case 2: return (float32)(2 / (1 + math.Pow(math.E, (float64)(-2 * value))) - 1) // Tanh - гиперболический тангенс
 	}
 }
 
 // Функция производной активации
-func getDerivative(val float32, mode uint8) float32 {
+func GetDerivative(value float32, mode uint8) float32 {
 	switch mode {
 	default: fallthrough
-	case 0: return val * (1 - val)
+	case 0: return value * (1 - value)
 	case 1: return 1
 	}
 }
 
 // Функция вывода результатов нейросети
-func (matrix *NNMatrix) printMatrix(collision float32) {
+func (matrix *NNMatrix) PrintNN(collision float32) {
 	t := "Layer"
 	n := matrix.Size - 1
 	for i := 0; i < matrix.Size; i++ {
@@ -210,4 +221,14 @@ func (matrix *NNMatrix) printMatrix(collision float32) {
 		fmt.Println(matrix.Weight[i].Weight)
 	}
 	fmt.Println("Total Error:\t", collision)
+}
+
+//
+func (matrix *NNMatrix) WriteWeight() error {
+	message := []byte(strconv.FormatFloat((float64)(matrix.Weight[0].Weight[0][0]), 'f', -1, 32))
+	err := ioutil.WriteFile("weight.data", message, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return err
 }
